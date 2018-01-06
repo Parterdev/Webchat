76a9c7f43a0aaa905942913e332a31488dc01d01
Parterjav <litemeilateran@outlook.com>
2018-01-05 23:27:09 -0500

    Eliminación de carpeta con datos obsoletos del proyecto

--- modelo-capa-vista/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost","root","12345678","Webchat");
-  if(!$conexion) {
-    echo 'Error al conectar la base de datos.';
-  }
-  else {
-    echo 'Conexion satisfactoria a la base de datos.';
-  }
-?>
